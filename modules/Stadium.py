5ceda6ae8968304e42d8bbd8633b0999a07f2142
KuhaarTar <flempiefilms@gmail.com>
2023-05-21 13:38:51 +0300

    create modules packcage

--- modules/Stadium.py
+++ modules/Stadium.py
@@ -1,7 +1,4 @@
-from lab8.modules.AbstractStadium import AbstractStadium
-
-
-class Stadium(AbstractStadium):
+class Stadium:
     """
     That class describe Stadium with methods:
     get_instance(),add_attends(),decrease_attendance(),change_home_team(),change_away_team()
@@ -17,18 +14,20 @@ class Stadium(AbstractStadium):
         :param home_team:
         :param away_team:
         """
-        super().__init__(name, capacity, current_attendance)
-        self.home_team = home_team
-        self.away_team = away_team
+        self.__name = name
+        self.__capacity = capacity
+        self.__current_attendance = current_attendance
+        self.__home_team = home_team
+        self.__away_team = away_team
 
     def __str__(self):
         """
         The method returns a string view of object
         :return: string view of object
         """
-        return f"Stadium(name={self.name}, capacity={self.capacity}, " \
-               f"current_attendance={self.current_attendance}, home_team={self.home_team}, " \
-               f"away_team={self.away_team})"
+        return f"Stadium(name={self.__name}, capacity={self.__capacity}, " \
+               f"current_attendance={self.__current_attendance}, home_team={self.__home_team}, " \
+               f"away_team={self.__away_team})"
 
     def __repr__(self):
         """
@@ -37,6 +36,86 @@ class Stadium(AbstractStadium):
         """
 
         return "test repr"
+
+    @property
+    def away_team(self):
+        """
+        Method return __away_team field value
+        :return: __away_team
+        """
+        return self.__away_team
+
+    @away_team.setter
+    def away_team(self, away_team):
+        """
+        Sets the away_team field value
+        :param away_team:
+        :return:
+        """
+        self.__away_team = away_team
+
+    @property
+    def home_team(self):
+        """
+        Method return __home_team field value
+        :return: __home_team
+        """
+        return self.__home_team
+
+    @home_team.setter
+    def home_team(self, home_team):
+        """
+        Sets the home_team field value
+        :param home_team:
+        :return:
+        """
+        self.__home_team = home_team
+
+    @property
+    def current_attendance(self):
+        """
+        Return __current_attendance filed value
+        :return:
+        """
+        return self.__current_attendance
+
+    @current_attendance.setter
+    def current_attendance(self, current_attendance):
+        self.__current_attendance = current_attendance
+
+    @property
+    def capacity(self):
+        """
+        Method return field __capacity value
+        :return: __capacity
+        """
+        return self.__capacity
+
+    @capacity.setter
+    def capacity(self, capacity):
+        """
+        Method set filed value
+        :param capacity:
+        :return:
+        """
+        self.__capacity = capacity
+
+    @property
+    def name(self):
+        """
+        method return field value
+        :return: __name
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        method set field value
+        :param name:
+        :return:
+        """
+        self.__name = name
 
     @staticmethod
     def get_instance():
@@ -48,33 +127,26 @@ class Stadium(AbstractStadium):
             Stadium.__instance = Stadium()
         return Stadium.__instance
 
-    def get_supported_sports(self):
-        """
-        overriding abstract method
-        :return:Returns a list of sports that can be contested at this sports complex
-        """
-        return ['Football', 'Basketball', 'Volleyball']
-
     def add_attends(self, count):
         """
         method increase current_attendance if there are free places
         :param count:
         :return:
         """
-        if self.current_attendance + count > self.__capacity:
+        if self.__current_attendance + count > self.__capacity:
             print('There are no vacant seats')
         else:
-            self.current_attendance += count
+            self.__current_attendance += count
 
     def decrease_attendance(self):
         """
         method decrease attendance by 100
         :return:
         """
-        if self.current_attendance - 100 < 0:
+        if self.__current_attendance - 100 < 0:
             print('There are not so many visitors')
         else:
-            self.current_attendance -= 100
+            self.__current_attendance -= 100
 
     def change_home_team(self, new_home_team):
         """
@@ -82,7 +154,7 @@ class Stadium(AbstractStadium):
         :param new_home_team:
         :return:
         """
-        self.home_team = new_home_team
+        self.__home_team = new_home_team
 
     def change_away_team(self, new_away_team):
         """
@@ -90,4 +162,13 @@ class Stadium(AbstractStadium):
         :param new_away_team:
         :return:
         """
-        self.away_team = new_away_team
+        self.__away_team = new_away_team
+
+
+stadiums = [Stadium(),
+            Stadium('Silmash', 10000, 45, "Lviv red team", "Blue team"),
+            Stadium.get_instance(),
+            Stadium.get_instance()]
+
+for item in stadiums:
+    print(item)
