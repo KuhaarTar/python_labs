48a9a168feaaa981bac02f12d7d6e6bb0694d96b
KuhaarTar <flempiefilms@gmail.com>
2023-05-21 13:34:47 +0300

    reacreate java lab into python

---
+++ modules/Gym.py
@@ -0,0 +1,37 @@
+from lab8.modules.AbstractStadium import AbstractStadium
+
+
+class Gym(AbstractStadium):
+    def __init__(self, name, capacity, current_attendance, count_of_simulator, monthly_subscription_fee, count_of_coach):
+        """
+        Initializes a Gym object.
+
+        :param name: The name of the gym.
+        :param capacity: The capacity of the gym.
+        :param current_attendance: The current attendance of the gym.
+        :param count_of_simulator: The count of simulators in the gym.
+        :param monthly_subscription_fee: The monthly subscription fee for the gym.
+        :param count_of_coach: The count of coaches in the gym.
+        """
+        super().__init__(name, capacity, current_attendance)
+        self.count_of_simulator = count_of_simulator
+        self.monthly_subscription_fee = monthly_subscription_fee
+        self.count_of_coach = count_of_coach
+
+    def get_supported_sports(self):
+        """
+        Returns the list of supported sports for the gym.
+
+        :return: The list of supported sports for the gym.
+        """
+        return ['BOX']
+
+    def __str__(self):
+        """
+        Returns a string representation of the Gym object.
+
+        :return: String representation of the Gym object.
+        """
+        return f"{super().__str__()} Count of Simulator: {self.count_of_simulator} " \
+               f"Monthly Subscription Fee: {self.monthly_subscription_fee} " \
+               f"Count of Coach: {self.count_of_coach}"
---
+++ modules/Stadium.py
@@ -0,0 +1,93 @@
+from lab8.modules.AbstractStadium import AbstractStadium
+
+
+class Stadium(AbstractStadium):
+    """
+    That class describe Stadium with methods:
+    get_instance(),add_attends(),decrease_attendance(),change_home_team(),change_away_team()
+    """
+    __instance = None
+
+    def __init__(self, name=None, capacity=0, current_attendance=0, home_team=None, away_team=None):
+        """
+        This is constructor to init fileds of class
+        :param name:
+        :param capacity:
+        :param current_attendance:
+        :param home_team:
+        :param away_team:
+        """
+        super().__init__(name, capacity, current_attendance)
+        self.home_team = home_team
+        self.away_team = away_team
+
+    def __str__(self):
+        """
+        The method returns a string view of object
+        :return: string view of object
+        """
+        return f"Stadium(name={self.name}, capacity={self.capacity}, " \
+               f"current_attendance={self.current_attendance}, home_team={self.home_team}, " \
+               f"away_team={self.away_team})"
+
+    def __repr__(self):
+        """
+        The method returns a string view of object
+        :return: string view of object
+        """
+
+        return "test repr"
+
+    @staticmethod
+    def get_instance():
+        """
+        Method return instance of Stadium class
+        :return: __instance
+        """
+        if Stadium.__instance is None:
+            Stadium.__instance = Stadium()
+        return Stadium.__instance
+
+    def get_supported_sports(self):
+        """
+        overriding abstract method
+        :return:Returns a list of sports that can be contested at this sports complex
+        """
+        return ['Football', 'Basketball', 'Volleyball']
+
+    def add_attends(self, count):
+        """
+        method increase current_attendance if there are free places
+        :param count:
+        :return:
+        """
+        if self.current_attendance + count > self.__capacity:
+            print('There are no vacant seats')
+        else:
+            self.current_attendance += count
+
+    def decrease_attendance(self):
+        """
+        method decrease attendance by 100
+        :return:
+        """
+        if self.current_attendance - 100 < 0:
+            print('There are not so many visitors')
+        else:
+            self.current_attendance -= 100
+
+    def change_home_team(self, new_home_team):
+        """
+        change home_team in stadium
+        :param new_home_team:
+        :return:
+        """
+        self.home_team = new_home_team
+
+    def change_away_team(self, new_away_team):
+        """
+        change away_team in stadium
+        :param new_away_team:
+        :return:
+        """
+        self.away_team = new_away_team
